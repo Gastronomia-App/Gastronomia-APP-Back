f3ef6800ed1f2fdf15e8f20f2532bb310037f747
Francesco <141887009+francesco-biondi@users.noreply.github.com>
2025-11-02 15:13:37 -0300

    Refactor product relationships and cleanup logic
    Introduces usedInProductOptions to Product entity and adds a bulk delete method to ProductOptionRepository. Refactors ProductService to centralize relationship updates and ensures proper cleanup of related entities when deleting a product, improving maintainability and data integrity.

--- src/main/java/com/progra3/cafeteria_api/model/entity/Product.java
+++ src/main/java/com/progra3/cafeteria_api/model/entity/Product.java
@@ -50,6 +50,9 @@ public class Product {
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<ProductComponent> usedInProducts = new HashSet<>();
 
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<ProductOption> usedInProductOptions = new HashSet<>();
+
     @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinTable(
             name = "groups_by_products",
